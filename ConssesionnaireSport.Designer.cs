42868ff5b6d99a6a7ed0e3dccfed8d5b3cdf92f4
Ava <avanastar3@gmail.com>
2024-12-02 10:55:43 -0400

    init

---
+++ ConssesionnaireSport.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace ConcessionnaireSport
+{
+    partial class ConssesionnaireSport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imagePictureBox = new System.Windows.Forms.PictureBox();
+            this.transactionMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.transactionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoClientGroupBox = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.courrielLabel = new System.Windows.Forms.Label();
+            this.nomLabel = new System.Windows.Forms.Label();
+            this.transactionGroupBox = new System.Windows.Forms.GroupBox();
+            this.prixTotalLabel = new System.Windows.Forms.Label();
+            this.prixProduitLabel = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.prixLabel = new System.Windows.Forms.Label();
+            this.produitLabel = new System.Windows.Forms.Label();
+            this.fournisseurLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).BeginInit();
+            this.transactionMenuStrip.SuspendLayout();
+            this.infoClientGroupBox.SuspendLayout();
+            this.transactionGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imagePictureBox
+            // 
+            this.imagePictureBox.Image = global::ConcessionnaireProche.Properties.Resources.superCarsImage;
+            this.imagePictureBox.Location = new System.Drawing.Point(25, 39);
+            this.imagePictureBox.Name = "imagePictureBox";
+            this.imagePictureBox.Size = new System.Drawing.Size(530, 281);
+            this.imagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imagePictureBox.TabIndex = 0;
+            this.imagePictureBox.TabStop = false;
+            // 
+            // transactionMenuStrip
+            // 
+            this.transactionMenuStrip.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.transactionMenuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.transactionMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.transactionToolStripMenuItem});
+            this.transactionMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.transactionMenuStrip.Name = "transactionMenuStrip";
+            this.transactionMenuStrip.Size = new System.Drawing.Size(579, 33);
+            this.transactionMenuStrip.TabIndex = 1;
+            this.transactionMenuStrip.Text = "menuStrip1";
+            // 
+            // transactionToolStripMenuItem
+            // 
+            this.transactionToolStripMenuItem.Name = "transactionToolStripMenuItem";
+            this.transactionToolStripMenuItem.Size = new System.Drawing.Size(116, 29);
+            this.transactionToolStripMenuItem.Text = "Transaction";
+            // 
+            // infoClientGroupBox
+            // 
+            this.infoClientGroupBox.Controls.Add(this.textBox2);
+            this.infoClientGroupBox.Controls.Add(this.textBox1);
+            this.infoClientGroupBox.Controls.Add(this.courrielLabel);
+            this.infoClientGroupBox.Controls.Add(this.nomLabel);
+            this.infoClientGroupBox.Location = new System.Drawing.Point(25, 326);
+            this.infoClientGroupBox.Name = "infoClientGroupBox";
+            this.infoClientGroupBox.Size = new System.Drawing.Size(530, 158);
+            this.infoClientGroupBox.TabIndex = 3;
+            this.infoClientGroupBox.TabStop = false;
+            this.infoClientGroupBox.Text = "Information du client";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(179, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(302, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(179, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(302, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // courrielLabel
+            // 
+            this.courrielLabel.AutoSize = true;
+            this.courrielLabel.Location = new System.Drawing.Point(45, 97);
+            this.courrielLabel.Name = "courrielLabel";
+            this.courrielLabel.Size = new System.Drawing.Size(67, 20);
+            this.courrielLabel.TabIndex = 1;
+            this.courrielLabel.Text = "Courriel:";
+            // 
+            // nomLabel
+            // 
+            this.nomLabel.AutoSize = true;
+            this.nomLabel.Location = new System.Drawing.Point(45, 46);
+            this.nomLabel.Name = "nomLabel";
+            this.nomLabel.Size = new System.Drawing.Size(46, 20);
+            this.nomLabel.TabIndex = 0;
+            this.nomLabel.Text = "Nom:";
+            // 
+            // transactionGroupBox
+            // 
+            this.transactionGroupBox.Controls.Add(this.prixTotalLabel);
+            this.transactionGroupBox.Controls.Add(this.prixProduitLabel);
+            this.transactionGroupBox.Controls.Add(this.textBox5);
+            this.transactionGroupBox.Controls.Add(this.textBox4);
+            this.transactionGroupBox.Controls.Add(this.textBox3);
+            this.transactionGroupBox.Controls.Add(this.totalLabel);
+            this.transactionGroupBox.Controls.Add(this.prixLabel);
+            this.transactionGroupBox.Controls.Add(this.produitLabel);
+            this.transactionGroupBox.Controls.Add(this.fournisseurLabel);
+            this.transactionGroupBox.Controls.Add(this.dateLabel);
+            this.transactionGroupBox.Location = new System.Drawing.Point(25, 506);
+            this.transactionGroupBox.Name = "transactionGroupBox";
+            this.transactionGroupBox.Size = new System.Drawing.Size(530, 312);
+            this.transactionGroupBox.TabIndex = 4;
+            this.transactionGroupBox.TabStop = false;
+            this.transactionGroupBox.Text = "Transaction";
+            // 
+            // prixTotalLabel
+            // 
+            this.prixTotalLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.prixTotalLabel.Location = new System.Drawing.Point(327, 261);
+            this.prixTotalLabel.Name = "prixTotalLabel";
+            this.prixTotalLabel.Size = new System.Drawing.Size(154, 31);
+            this.prixTotalLabel.TabIndex = 10;
+            this.prixTotalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // prixProduitLabel
+            // 
+            this.prixProduitLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.prixProduitLabel.Location = new System.Drawing.Point(327, 206);
+            this.prixProduitLabel.Name = "prixProduitLabel";
+            this.prixProduitLabel.Size = new System.Drawing.Size(154, 31);
+            this.prixProduitLabel.TabIndex = 9;
+            this.prixProduitLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(179, 153);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(302, 26);
+            this.textBox5.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(179, 98);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(302, 26);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(179, 43);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(302, 26);
+            this.textBox3.TabIndex = 4;
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(45, 266);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(48, 20);
+            this.totalLabel.TabIndex = 6;
+            this.totalLabel.Text = "Total:";
+            // 
+            // prixLabel
+            // 
+            this.prixLabel.AutoSize = true;
+            this.prixLabel.Location = new System.Drawing.Point(45, 211);
+            this.prixLabel.Name = "prixLabel";
+            this.prixLabel.Size = new System.Drawing.Size(42, 20);
+            this.prixLabel.TabIndex = 5;
+            this.prixLabel.Text = "Prix: ";
+            // 
+            // produitLabel
+            // 
+            this.produitLabel.AutoSize = true;
+            this.produitLabel.Location = new System.Drawing.Point(45, 156);
+            this.produitLabel.Name = "produitLabel";
+            this.produitLabel.Size = new System.Drawing.Size(64, 20);
+            this.produitLabel.TabIndex = 4;
+            this.produitLabel.Text = "Voiture:";
+            // 
+            // fournisseurLabel
+            // 
+            this.fournisseurLabel.AutoSize = true;
+            this.fournisseurLabel.Location = new System.Drawing.Point(45, 101);
+            this.fournisseurLabel.Name = "fournisseurLabel";
+            this.fournisseurLabel.Size = new System.Drawing.Size(60, 20);
+            this.fournisseurLabel.TabIndex = 3;
+            this.fournisseurLabel.Text = "Livreur:";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Location = new System.Drawing.Point(45, 46);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(48, 20);
+            this.dateLabel.TabIndex = 2;
+            this.dateLabel.Text = "Date:";
+            // 
+            // ConssesionnaireSport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 830);
+            this.Controls.Add(this.transactionGroupBox);
+            this.Controls.Add(this.infoClientGroupBox);
+            this.Controls.Add(this.imagePictureBox);
+            this.Controls.Add(this.transactionMenuStrip);
+            this.MainMenuStrip = this.transactionMenuStrip;
+            this.Name = "ConssesionnaireSport";
+            this.Text = "Concessionnaire Voiture Sport";
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).EndInit();
+            this.transactionMenuStrip.ResumeLayout(false);
+            this.transactionMenuStrip.PerformLayout();
+            this.infoClientGroupBox.ResumeLayout(false);
+            this.infoClientGroupBox.PerformLayout();
+            this.transactionGroupBox.ResumeLayout(false);
+            this.transactionGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imagePictureBox;
+        private System.Windows.Forms.MenuStrip transactionMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem transactionToolStripMenuItem;
+        private System.Windows.Forms.GroupBox infoClientGroupBox;
+        private System.Windows.Forms.GroupBox transactionGroupBox;
+        private System.Windows.Forms.Label courrielLabel;
+        private System.Windows.Forms.Label nomLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label prixLabel;
+        private System.Windows.Forms.Label produitLabel;
+        private System.Windows.Forms.Label fournisseurLabel;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label prixTotalLabel;
+        private System.Windows.Forms.Label prixProduitLabel;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
